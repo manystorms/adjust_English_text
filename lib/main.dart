969f068f2c286744bdc3dbead3a8726fa8e229ff
manystorms <kimhayul2007@naver.com>
2024-05-27 17:48:30 +0900

    asdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:serious_python/serious_python.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,19 +9,59 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Text Widget Example'),
-        ),
-        body: Center(
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _output = "Press the button to run Python code";
+
+  void _runPython(int a, int b) async {
+    try {
+      var result = await SeriousPython.run("assets/test.zip",
+          appFileName: "test.py",
+          environmentVariables: {"a": "1", "b": "2"});
+      print('a');
+      print(result);
+      setState(() {
+        if(result != null) _output = result;
+      });
+    } catch (e) {
+      setState(() {
+        _output = "Error: $e";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int a = 3;
+    int b = 4;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Serious Python Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _output,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 24),
             ),
-          ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => _runPython(a, b),
+              child: Text('Run Python Code'),
+            ),
+          ],
         ),
       ),
     );
